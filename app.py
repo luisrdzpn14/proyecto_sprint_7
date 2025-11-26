25d022b9bdb671e539fba528f67d0c57b9b36266
luisrdzpn14 <ponyrz@gmail.com>
2025-11-25 22:23:31 -0600

    subir url app

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+# Leer los datos
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Encabezado de la aplicación
+st.header('Análisis de Datos de Vehículos Usados')
+
+# Botón para histograma
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
